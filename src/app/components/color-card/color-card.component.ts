61983087b9180e7629734397c604b4d3fdb1323c
sch-9 <laschleichgarcia@estudiantes.unsam.edu.ar>
2023-12-09 10:02:29 -0300

    popup'´options

--- src/app/components/color-card/color-card.component.ts
+++ src/app/components/color-card/color-card.component.ts
@@ -8,4 +8,18 @@ import { ColorCard } from 'src/domain/ColorCard'
 })
 export class ColorCardComponent {
   @Input() colorCard!: ColorCard
+  enabled = false
+
+  changeEnabled(value: boolean){ this.enabled = value }
+
+  handleBlur(e: FocusEvent){
+    const relatedTargetIds = ['color-card__options', 'color-card__remove', 'color-card__edit']
+    const relatedTarget = (e.relatedTarget as HTMLElement)
+    const relatedTargetId = relatedTarget? relatedTarget.id : 'noo'
+    
+    if (!e.relatedTarget || !relatedTargetIds.includes(relatedTargetId)) {
+        this.enabled = false
+        console.log("aaaaaaaa", relatedTargetId, relatedTargetId in relatedTargetIds)
+    }
+  }
 }
