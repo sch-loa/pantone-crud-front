52dd6770009bf8dfdda1bc64b6ed0752f93c984c
sch-9 <laschleichgarcia@estudiantes.unsam.edu.ar>
2023-12-10 08:17:49 -0300

    checkbox tests

--- src/app/components/shared/input/checkbox/checkbox.component.spec.ts
+++ src/app/components/shared/input/checkbox/checkbox.component.spec.ts
@@ -3,15 +3,16 @@ import { DECLARATIONS, IMPORTS } from 'src/app/app.module'
 import { CheckboxComponent } from './checkbox.component'
 
 describe('CheckboxComponent', () => {
-  let component: CheckboxComponent
-  let fixture: ComponentFixture<CheckboxComponent>
+  let component: CheckboxComponentStub
+  let fixture: ComponentFixture<CheckboxComponentStub>
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CheckboxComponent, DECLARATIONS],
+      declarations: [CheckboxComponentStub, DECLARATIONS],
       imports: IMPORTS
     })
-    fixture = TestBed.createComponent(CheckboxComponent)
+
+    fixture = TestBed.createComponent(CheckboxComponentStub)
     component = fixture.componentInstance
     fixture.detectChanges()
   })
@@ -19,4 +20,39 @@ describe('CheckboxComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy()
   })
+
+  it('should emit false state change initially', () => {
+    expect(component.state).toBeFalsy()
+    expect(component.eventEmitted).toBeFalsy()
+  })
+
+  it('should emit true state change', () => {
+    const checkbox = searchElement('checkbox')
+    checkbox.click()
+    fixture.detectChanges()
+    expect(component.state).toBeTruthy()
+    expect(component.eventEmitted).toBeTruthy()
+  })
+
+  it('should emit false state change', () => {
+    const checkbox = searchElement('checkbox')
+    checkbox.click()
+    fixture.detectChanges()
+    checkbox.click()
+    fixture.detectChanges()
+    expect(component.state).toBeFalsy()
+    expect(component.eventEmitted).toBeFalsy()
+  })
+
+  function searchElement(testId: string) {
+    const compiled = fixture.debugElement.nativeElement
+    return compiled.querySelector(`[data-testid="${testId}"]`)
+  }
+
+  class CheckboxComponentStub extends CheckboxComponent{
+    eventEmitted!: boolean
+    override emitChangedEvent(): void {
+        this.eventEmitted = this.state
+    }
+  }
 })
--- src/app/components/shared/input/checkbox/checkbox.component.ts
+++ src/app/components/shared/input/checkbox/checkbox.component.ts
@@ -6,9 +6,7 @@ import { Component, Input, Output, EventEmitter, AfterViewInit } from '@angular/
   styleUrls: ['./checkbox.component.css']
 })
 export class CheckboxComponent implements AfterViewInit{
-  @Input() paramName!: string
   @Input() state!: boolean
-  @Input() timeOut!: number
   @Output() changedEvent = new EventEmitter<boolean>()
 
   ngAfterViewInit() { setTimeout(() => this.emitChangedEvent()) }
