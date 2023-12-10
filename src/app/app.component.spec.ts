ad7c14dd42fefc61cc8e8c4eb1c99adbf58179b9
sch-9 <laschleichgarcia@estudiantes.unsam.edu.ar>
2023-12-10 07:58:08 -0300

    fix lint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,3 @@
-import './app.module'
-
 import { TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
